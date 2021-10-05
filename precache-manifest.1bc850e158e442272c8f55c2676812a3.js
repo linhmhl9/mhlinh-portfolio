18af306c606b28249201c170eb8a629b725b322d
linhmhl9 <linh.mhl9@gmail.com>
2021-10-05 10:52:37 +0700

    Updates

---
+++ precache-manifest.1bc850e158e442272c8f55c2676812a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a860f1ba740af71dae0aee8d07bb9d1",
+    "url": "/mhlinh-portfolio/index.html"
+  },
+  {
+    "revision": "f53f7f1ce5170da2f945",
+    "url": "/mhlinh-portfolio/static/css/2.dfb7c7bb.chunk.css"
+  },
+  {
+    "revision": "b1212e1f80684c6a1889",
+    "url": "/mhlinh-portfolio/static/css/main.bad3cd24.chunk.css"
+  },
+  {
+    "revision": "f53f7f1ce5170da2f945",
+    "url": "/mhlinh-portfolio/static/js/2.593ec0c7.chunk.js"
+  },
+  {
+    "revision": "14aecb71cf7b7d04ddf7eb6a3f6cae32",
+    "url": "/mhlinh-portfolio/static/js/2.593ec0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1212e1f80684c6a1889",
+    "url": "/mhlinh-portfolio/static/js/main.6e54720c.chunk.js"
+  },
+  {
+    "revision": "368a6c2ba6f41aaed700",
+    "url": "/mhlinh-portfolio/static/js/runtime-main.4a997f06.js"
+  }
+]);
